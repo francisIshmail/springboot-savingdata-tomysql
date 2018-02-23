4109365762f69778e556cb78c197beb47ac92aff
Ishmail <francis.ishmael@compulynx.com>
2018-02-23 15:25:35 +0300

    Listing all users

--- src/main/java/com/devsafrica/service/UserService.java
+++ src/main/java/com/devsafrica/service/UserService.java
@@ -4,8 +4,12 @@ package com.devsafrica.service;
 import com.devsafrica.domain.User;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public interface UserService {
+
+    List<User> getAllUsers();
 
     User getUserById(Long id);
 
